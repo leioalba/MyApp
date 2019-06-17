f512416a8da2818e72d1eb066c21e569d07c7a6c
leioalba <294075195@qq.com>
2019-06-17 10:27:50 +0800

    侧滑

--- app/src/main/java/cn/edu/gdpt/androiddemo/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/androiddemo/MainActivity.java
@@ -1,13 +1,25 @@
 package cn.edu.gdpt.androiddemo;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.menu_main);
+        drawerLayout=findViewById(R.id.dl_left);
     }
+    
+    private void showDrawerLayout() {
+        if (!drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            drawerLayout.openDrawer(Gravity.LEFT);
+        } else {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+        }
+    }
+
 }
