9faa31a3ff124c99cc6e528528ae0a1494be91c6
HoneKKK <729160059@qq.com>
2019-06-17 11:51:55 +0800

    listview

---
+++ app/src/main/java/cn/edu/gdpt/androiddemo/Adapter/FragmentPagerAdapter.java
@@ -0,0 +1,34 @@
+package cn.edu.gdpt.androiddemo.Adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import cn.edu.gdpt.androiddemo.Fragment.CoverterFragment;
+import cn.edu.gdpt.androiddemo.Fragment.HomeFragment;
+
+public class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
+    private String[] mTitles = new String[]{"市场汇率", "汇率转换"};
+    public FragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        if (i == 1)
+            return new CoverterFragment();
+
+        return new HomeFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return mTitles.length;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+}
--- app/src/main/java/cn/edu/gdpt/androiddemo/MainActivity.java
+++ app/src/main/java/cn/edu/gdpt/androiddemo/MainActivity.java
@@ -3,16 +3,21 @@ package cn.edu.gdpt.androiddemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 
+import cn.edu.gdpt.androiddemo.Adapter.FragmentPagerAdapter;
+
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private ImageView img_main_menu;
     private TabLayout tablayout_main;
+    private ViewPager viewPager;
+    private FragmentPagerAdapter fragmentPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +37,13 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         img_main_menu = (ImageView) findViewById(R.id.img_main_menu);
         tablayout_main = (TabLayout) findViewById(R.id.tablayout_main);
+        viewPager = (ViewPager) findViewById(R.id.viewPager_main);
         drawerLayout = findViewById(R.id.dl_left);
 
-        tablayout_main.addTab(tablayout_main.newTab().setText("市场汇率"));
-        tablayout_main.addTab(tablayout_main.newTab().setText("汇率转换"));
+        fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragmentPagerAdapter);
+        tablayout_main.setupWithViewPager(viewPager);
+
         img_main_menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
